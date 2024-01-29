8a72abda5a32cba2b8d2adcdaed72c3cb4f0f9cb
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-01-29 13:41:44 -0600

    enable debug mode for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 import 'package:holodex_plus_flutter/app.dart';
 
-void main() {
+void main() async {
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+    await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
+  }
   runApp(const App());
 }
