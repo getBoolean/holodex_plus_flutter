122809d5336907bc45342b226791336dd469d845
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-01-28 21:16:07 -0600

    refactor intercepts

--- lib/web_view.dart
+++ lib/web_view.dart
@@ -103,86 +103,13 @@ class _WebViewState extends State<WebView> {
                     //   print(ajaxRequest.url);
                     //   return ajaxRequest;
                     // },
-                    shouldInterceptFetchRequest:
-                        (controller, fetchRequest) async => fetchRequest,
                     // shouldInterceptFetchRequest:
-                    //     (controller, request) async {
-                    //   print('INTERCEPTING FETCH REQUEST');
-                    //   final uri = request.url;
-                    //   if (uri == null) return request;
-                    //   if (!uri.path.startsWith(
-                    //     'https://www.youtube.com/redirect_replay_chat?',
-                    //   )) {
-                    //     print('SKIPPING FETCH REQUEST');
-                    //     return request;
-                    //   }
-                    //   final videoId = uri.queryParameters['v'];
-                    //   if (videoId == null) {
-                    //     print('SKIPPING FETCH REQUEST');
-                    //     return request;
-                    //   }
-                    //   final channelId = uri.queryParameters['c'];
-                    //   if (channelId == null) {
-                    //     print('SKIPPING FETCH REQUEST');
-                    //     return request;
-                    //   }
-                    //   final darkTheme = uri.queryParameters['dark_theme'];
-                    //   // final continuation = videoId != null &&
-                    //   //     channelId != null &&
-                    //   //     rrc(
-                    //   //       videoId: videoId,
-                    //   //       channelId: channelId,
-                    //   //     );
-                    //   final redirect = WebUri(
-                    //     'https://www.youtube.com/live_chat_replay',
-                    //   );
-                    //   if (darkTheme != null) {
-                    //     redirect.queryParameters['dark_theme'] = darkTheme;
-                    //   }
-                    //   print('REDIRECTING FETCH REQUEST');
-                    //   return FetchRequest(
-                    //     url: redirect,
-                    //     method: request.method,
-                    //     headers: request.headers
-                    //       ?..removeWhere(
-                    //         (key, value) =>
-                    //             key.toLowerCase() == 'x-frame-options',
-                    //       ),
-                    //     body: request.body,
-                    //     mode: request.mode,
-                    //     credentials: request.credentials,
-                    //     cache: request.cache,
-                    //   );
-                    // },
+                    //     (controller, fetchRequest) async => fetchRequest,
+                    shouldInterceptFetchRequest: (controller, request) async =>
+                        await interceptRequest(request),
                     shouldOverrideUrlLoading:
-                        (controller, navigationAction) async {
-                      final uri = navigationAction.request.url;
-                      if (uri == null) {
-                        return NavigationActionPolicy.ALLOW;
-                      }
-
-                      if (![
-                        'http',
-                        'https',
-                        'file',
-                        'chrome',
-                        'data',
-                        'javascript',
-                        'about',
-                      ].contains(uri.scheme)) {
-                        if (await canLaunchUrl(uri)) {
-                          // Launch the App
-                          await launchUrl(
-                            uri,
-                          );
-                          // and cancel the request
-
-                          return NavigationActionPolicy.CANCEL;
-                        }
-                      }
-
-                      return NavigationActionPolicy.ALLOW;
-                    },
+                        (controller, navigationAction) async =>
+                            await launchSupportedUrls(navigationAction),
                     onLoadStop: (controller, url) async {
                       await pullToRefreshController?.endRefreshing();
                       setState(() {
@@ -225,6 +152,99 @@ class _WebViewState extends State<WebView> {
           ],
         ),
       ),
+    );
+  }
+
+  Future<NavigationActionPolicy> launchSupportedUrls(
+    NavigationAction navigationAction,
+  ) async {
+    final uri = navigationAction.request.url;
+    if (uri == null) {
+      return NavigationActionPolicy.ALLOW;
+    }
+
+    if (![
+      'http',
+      'https',
+      'file',
+      'chrome',
+      'data',
+      'javascript',
+      'about',
+    ].contains(uri.scheme)) {
+      if (await canLaunchUrl(uri)) {
+        await launchUrl(uri);
+
+        return NavigationActionPolicy.CANCEL;
+      }
+    }
+
+    return NavigationActionPolicy.ALLOW;
+  }
+
+  Future<FetchRequest> interceptRequest(FetchRequest request) async {
+    debugPrint('INTERCEPTING FETCH REQUEST');
+    final uri = request.url;
+    if (uri == null) return request;
+    if (uri.rawValue.startsWith(
+      'https://www.youtube.com/redirect_replay_chat?',
+    )) {
+      return _handleRedirectReplayChat(uri, request);
+    } else if (uri.rawValue.contains('youtube.com/live_chat_replay?')) {
+      return _handleLiveChatReplay(request);
+    }
+
+    debugPrint('SKIPPING FETCH REQUEST');
+
+    return request;
+  }
+
+  FetchRequest _handleLiveChatReplay(FetchRequest request) {
+    return request
+      ..headers?.removeWhere(
+        (key, value) => key.toLowerCase() == 'x-frame-options',
+      );
+  }
+
+  FetchRequest _handleRedirectReplayChat(WebUri uri, FetchRequest request) {
+    final videoId = uri.queryParameters['v'];
+    if (videoId == null) {
+      debugPrint('SKIPPING FETCH REQUEST');
+
+      return request;
+    }
+    final channelId = uri.queryParameters['c'];
+    if (channelId == null) {
+      debugPrint('SKIPPING FETCH REQUEST');
+
+      return request;
+    }
+    final darkTheme = uri.queryParameters['dark_theme'];
+    // final continuation = videoId != null &&
+    //     channelId != null &&
+    //     rrc(
+    //       videoId: videoId,
+    //       channelId: channelId,
+    //     );
+    final redirect = WebUri(
+      'https://www.youtube.com/live_chat_replay',
+    );
+    if (darkTheme != null) {
+      redirect.queryParameters['dark_theme'] = darkTheme;
+    }
+    debugPrint('REDIRECTING FETCH REQUEST');
+
+    return FetchRequest(
+      url: redirect,
+      method: request.method,
+      headers: request.headers
+        ?..removeWhere(
+          (key, value) => key.toLowerCase() == 'x-frame-options',
+        ),
+      body: request.body,
+      mode: request.mode,
+      credentials: request.credentials,
+      cache: request.cache,
     );
   }
 }
