02f79a350e168c62e6be2c79da59be8a22648525
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-01-28 19:19:53 -0600

    enable useShouldInterceptFetchRequest

--- lib/web_view.dart
+++ lib/web_view.dart
@@ -21,7 +21,7 @@ class _WebViewState extends State<WebView> {
     allowsInlineMediaPlayback: true,
     iframeAllow: 'camera; microphone',
     iframeAllowFullscreen: true,
-    useShouldInterceptFetchRequest: false,
+    useShouldInterceptFetchRequest: true,
     userAgent:
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
   );
